891c4d618f5db8fa30c7ba4cb031b8c7e9987905
AchillesGit <dennisadiki@gmail.com>
2024-12-05 14:00:22 +0100

    Implement solution for Day Five 2024: handle invalid prints and enforce order rules

--- 2024/DayFive.js
+++ 2024/DayFive.js
@@ -4,6 +4,7 @@ const filePath = path.join(__dirname, 'DayFiveInput.txt');
 
 let orderRules = [];
 let prints = [];
+let invalidPrints = [];
 let sum = 0;
 
 function parseOrderData() {
@@ -46,6 +47,8 @@ function checkCorrectOrders() {
     }
     if (lineIsValid) {
       handleCorrectLine(printLine);
+    } else {
+      invalidPrints.push(printLine);
     }
   }
 }
@@ -79,6 +82,70 @@ function handleCorrectLine(line) {
   sum += Number(line[Math.floor(lineLength / 2)]);
 }
 
+Array.prototype.move = function (from, to) {
+  this.splice(to, 0, this.splice(from, 1)[0]);
+};
+
+function checkLeftOrderRulesAndShift(printNumber, printIndex, printLine) {
+  const relevantRules = orderRules.filter((rule) => rule.left === printNumber);
+  const printsThatMustBeAfter = relevantRules.map((rule) => rule.right);
+  const printsThatAreBefore = [...printLine].splice(0, printIndex);
+  for (const print of printsThatAreBefore) {
+    if (printsThatMustBeAfter.includes(print)) {
+      const indexOfInvalid = printLine.findIndex((p) => p === print);
+      printLine.move(indexOfInvalid, printIndex);
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkRightOrderRulesAndShift(printNumber, printIndex, printLine) {
+  const relevantRules = orderRules.filter((rule) => rule.right === printNumber);
+  const printsThatMustBeBefore = relevantRules.map((rule) => rule.left);
+  const printsThatAreAfter = [...printLine].splice(printIndex + 1);
+  for (const print of printsThatAreAfter) {
+    if (printsThatMustBeBefore.includes(print)) {
+      const indexOfInvalid = printLine.findIndex((p) => p === print);
+      printLine.move(indexOfInvalid, printIndex);
+      return false;
+    }
+  }
+  return true;
+}
+
+function handleIncorrectPrints() {
+  for (let i = 0; i < invalidPrints.length; i++) {
+    const printLine = invalidPrints[i];
+    let lineIsValid = true;
+    for (let printIndex = 0; printIndex < printLine.length; printIndex++) {
+      const printNumber = printLine[printIndex];
+      const correctLeft = checkLeftOrderRulesAndShift(
+        printNumber,
+        printIndex,
+        printLine
+      );
+
+      const correctRight = checkRightOrderRulesAndShift(
+        printNumber,
+        printIndex,
+        printLine
+      );
+
+      if (!correctLeft || !correctRight) {
+        lineIsValid = false;
+      }
+    }
+    if (!lineIsValid) {
+      i--;
+    } else {
+      handleCorrectLine(printLine);
+    }
+  }
+}
+
 parseOrderData();
 checkCorrectOrders();
+sum = 0;
+handleIncorrectPrints();
 console.log(sum);
